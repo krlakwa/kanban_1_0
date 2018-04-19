8d6e5ccb259838720bc29678f70175a32310fa1c
Krzysztof Łakwa <fenrir14@gmail.com>
2018-04-19 15:23:10 +0200

    Add card function to js code

--- js/script.js
+++ js/script.js
@@ -59,7 +59,23 @@ $(function() {
         this.$element = createCard();
 
         function createCard() {
-            
+            var $card = $('<li>').addClass('card');
+            var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+            var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+            $cardDelete.on('click', function() {
+                self.removeCard();
+            });
+
+            $card.append($cardDelete)
+                .append($cardDescription);
+            return $card;
+        }
+    }
+
+    Card.prototype = {
+        removeCard: function() {
+            this.$element.remove();
         }
     }
 })
